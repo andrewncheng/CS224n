fixes KL divergence now loss is positive

VAE/VAE.py
@@ -9,16 +9,16 @@ class VAE(nn.Module):
         self.input_dim = input_dim
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.encoder = nn.Sequential(
-            nn.Linear(self.input_dim, 400),
+            nn.Linear(self.input_dim, 512),
             nn.ReLU(),
-            nn.Linear(400, 2 * self.hidden_dim),  # one for mean and one for variance
+            nn.Linear(512, self.hidden_dim + self.hidden_dim),  # one for mean and one for variance
             # nn.ReLU() ReLU only allows for non-negative values. We want to allow mu and sigma to also be negative
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(self.hidden_dim, 400),
+            nn.Linear(self.hidden_dim, 512),
             nn.ReLU(),
-            nn.Linear(400, self.input_dim),
+            nn.Linear(512, self.input_dim),
             # if we normalize our images to values between 0-1 then we should apply sigmoid to map values to 0-1
             nn.Sigmoid()
         )

VAE/train.py
@@ -19,7 +19,7 @@ input_dim = 784
 hidden_dim = 300
 num_epochs = 80
 batch_size = 128
-lr = 0.001
+lr = 1e-5
 
 # dataset loading
 dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
@@ -37,10 +37,13 @@ for epoch in tqdm(range(num_epochs)):
         #print("x_batch[0]", x_batch[0])
         x_reconstructed, mu, logvar = model(x_batch)
         reconstruction_loss = loss_fn(x_reconstructed, x_batch)
-        kl_divergence = torch.sum(mu**2 - logvar - 1 + torch.exp(0.5 * logvar))
+        kl_divergence = 0.5 * torch.sum(mu**2 + torch.exp(logvar) - logvar - 1)
         #print("KL_DIVERGENCE------", kl_divergence)
         optimizer.zero_grad()
         loss = reconstruction_loss + kl_divergence
+        if i %20 == 0:
+            print("RECONSTRUCTION LOSS", reconstruction_loss)
+            print("KL_DIVERGENCE", kl_divergence)
         loss.backward()
         optimizer.step()
         pbar.set_postfix(loss=loss.item())